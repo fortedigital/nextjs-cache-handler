2bb7c0b2d429b019c35b3afbd412a34141f3e3bc
Marcin Kotlicki <marcin.kotlicki@fortedigital.com>
2025-05-30 11:44:06 +0200

    Fixed ctor

--- packages/nextjs-cache-handler/src/handlers/next-15-cache-handler.ts
+++ packages/nextjs-cache-handler/src/handlers/next-15-cache-handler.ts
@@ -9,23 +9,25 @@ import {
  * https://github.com/caching-tools/next-shared-cache/issues/1027
  * https://github.com/vercel/next.js/commit/6a1b6336a3062dfdb1b9dddcee6b836da94fd198
  */
-export class Next15CacheHandler extends CacheHandler {
-  async set(
-    cacheKey: string,
-    incrementalCacheValue: IncrementalCacheValue | null,
-    ctx: {
-      revalidate?: number | false;
-      fetchCache?: boolean;
-      fetchUrl?: string;
-      fetchIdx?: number;
-      tags?: string[];
-    } & { neshca_lastModified?: number },
-  ) {
-    await super.set(cacheKey, incrementalCacheValue, {
-      ...ctx,
-      revalidate:
-        ctx.revalidate ||
-        (incrementalCacheValue as CachedFetchValue)?.revalidate,
-    });
-  }
+export function Next15CacheHandler() {
+  return class extends CacheHandler {
+    async set(
+      cacheKey: string,
+      incrementalCacheValue: IncrementalCacheValue | null,
+      ctx: {
+        revalidate?: number | false;
+        fetchCache?: boolean;
+        fetchUrl?: string;
+        fetchIdx?: number;
+        tags?: string[];
+      } & { neshca_lastModified?: number },
+    ) {
+      await super.set(cacheKey, incrementalCacheValue, {
+        ...ctx,
+        revalidate:
+          ctx.revalidate ||
+          (incrementalCacheValue as CachedFetchValue)?.revalidate,
+      });
+    }
+  };
 }
