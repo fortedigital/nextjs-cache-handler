6e5c59df60f2147f939912c86ce9cf02f16aa592
Marcin Kotlicki <marcin.kotlicki@fortedigital.com>
2025-05-30 16:01:21 +0200

    Refactoring and cleanup

--- packages/nextjs-cache-handler/src/handlers/redis-strings.ts
+++ packages/nextjs-cache-handler/src/handlers/redis-strings.ts
@@ -1,92 +1,14 @@
-import type { createClient } from "redis";
-
 import { REVALIDATED_TAGS_KEY } from "../constants";
 import { isImplicitTag } from "../helpers/isImplicitTag";
 import { CacheHandlerValue, Handler } from "./cache-handler.types";
 import { IncrementalCacheValue } from "next/dist/server/response-cache/types";
-
-export type CachedRouteValue = {
-  kind: "APP_ROUTE";
-  body: Buffer | undefined;
-};
-
-export type ConvertedCachedRouteValue = {
-  // See: https://github.com/vercel/next.js/blob/f5444a16ec2ef7b82d30048890b613aa3865c1f1/packages/next/src/server/response-cache/types.ts#L97
-  kind: "ROUTE";
-  body: string | undefined;
-};
-
-export type CachedAppPageValue = {
-  kind: "APP_PAGE";
-  rscData: Buffer | undefined;
-  segmentData: Map<string, Buffer> | undefined;
-};
-
-export type ConvertedCachedAppPageValue = {
-  // See: https://github.com/vercel/next.js/blob/f5444a16ec2ef7b82d30048890b613aa3865c1f1/packages/next/src/server/response-cache/types.ts#L76
-  kind: "PAGE";
-  rscData: string | undefined;
-  segmentData: Record<string, string> | undefined;
-};
-export type CreateRedisStringsHandlerOptions<
-  T = ReturnType<typeof createClient>,
-> = {
-  /**
-   * The Redis client instance.
-   */
-  client: T;
-  /**
-   * Optional. Prefix for all keys, useful for namespacing.
-   *
-   * @default '' // empty string
-   */
-  keyPrefix?: string;
-  /**
-   * Optional. Timeout in milliseconds for Redis operations.
-   *
-   * @default 5000 // 5000 ms
-   *
-   * @remarks
-   * To disable timeout of Redis operations, set this option to 0.
-   */
-  timeoutMs?: number;
-  /**
-   * Optional. The number of tags in a single query retrieved from Redis when scanning or searching for tags.
-   *
-   * @default 100 // 100 tags
-   *
-   * @remarks
-   * You can adjust this value to optimize the number of commands sent to Redis when scanning or searching for tags.
-   * A higher value will reduce the number of commands sent to Redis,
-   * but it will also increase the amount of data transferred over the network.
-   * Redis uses TCP and typically has 65,535 bytes as the maximum size of a packet (it can be lower depending on MTU).
-   */
-  revalidateTagQuerySize?: number;
-
-  /**
-   * Key for storing cache tags.
-   *
-   * @default '__sharedTags__'
-   */
-  sharedTagsKey?: string;
-  /**
-   * Key for storing cache tags TTL.
-   *
-   * @default '__sharedTagsTtl__'
-   */
-  sharedTagsTtlKey?: string;
-  /**
-   * Determines the expiration strategy for cache keys.
-   *
-   * - `'EXAT'`: Uses the `EXAT` option of the `SET` command to set expiration time.
-   * - `'EXPIREAT'`: Uses the `EXPIREAT` command to set expiration time.
-   *
-   * By default, it uses `'EXPIREAT'` for compatibility with older versions.
-   *
-   * @default 'EXPIREAT'
-   */
-  keyExpirationStrategy?: "EXAT" | "EXPIREAT";
-};
+import {
+  CreateRedisStringsHandlerOptions,
+  ConvertedCachedRouteValue,
+  CachedRouteValue,
+  ConvertedCachedAppPageValue,
+  CachedAppPageValue,
+} from "./redis-strings.types";
 
 /**
  * Creates a Handler for handling cache operations using Redis strings.
