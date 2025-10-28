0ac2becf35fbdfcbb90d1f6fbfc9b6b01231c42d
Fabian Koder <f.koder@durchblicker.at>
2025-10-28 15:51:57 +0100

    Populate page-cache in parallel instead of sequentially
    Previously, all routes were set one-by-one. Instead, up to `availableParallelism` pages are now concurrently read from the filesystem and populated into the cache handler.

--- packages/nextjs-cache-handler/src/instrumentation/register-initial-cache.ts
+++ packages/nextjs-cache-handler/src/instrumentation/register-initial-cache.ts
@@ -1,5 +1,6 @@
 import { promises as fsPromises } from "node:fs";
 import path from "node:path";
+import os from "node:os";
 import { PRERENDER_MANIFEST, SERVER_DIRECTORY } from "next/constants";
 import type { PrerenderManifest } from "next/dist/build";
 import { CACHE_ONE_YEAR } from "next/dist/lib/constants";
@@ -12,6 +13,7 @@ import {
 import type { OutgoingHttpHeaders } from "http";
 import { getTagsFromHeaders } from "../helpers/getTagsFromHeaders";
 import { Revalidate } from "../handlers/cache-handler.types";
+import pLimit from "p-limit";
 
 type CacheHandlerType = typeof import("../handlers/cache-handler").CacheHandler;
 
@@ -386,18 +388,22 @@ export async function registerInitialCache(
     }
   }
 
-  for (const [
-    cachePath,
-    { dataRoute, initialRevalidateSeconds },
-  ] of Object.entries(prerenderManifest.routes)) {
-    if (populatePages && dataRoute?.endsWith(".json")) {
-      await setPageCache(cachePath, "pages", initialRevalidateSeconds);
-    } else if (populatePages && dataRoute?.endsWith(".rsc")) {
-      await setPageCache(cachePath, "app", initialRevalidateSeconds);
-    } else if (populateRoutes && dataRoute === null) {
-      await setRouteCache(cachePath, "app", initialRevalidateSeconds);
-    }
-  }
+  const limit = pLimit(os.availableParallelism());
+
+  const promises = Object.entries(prerenderManifest.routes).map(
+    ([cachePath, { dataRoute, initialRevalidateSeconds }]) =>
+      limit(async () => {
+        if (populatePages && dataRoute?.endsWith(".json")) {
+          await setPageCache(cachePath, "pages", initialRevalidateSeconds);
+        } else if (populatePages && dataRoute?.endsWith(".rsc")) {
+          await setPageCache(cachePath, "app", initialRevalidateSeconds);
+        } else if (populateRoutes && dataRoute === null) {
+          await setRouteCache(cachePath, "app", initialRevalidateSeconds);
+        }
+      }),
+  );
+
+  await Promise.all(promises);
 
   if (!populateFetch) {
     return;
